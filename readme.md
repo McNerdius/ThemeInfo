8db6cdd67a624bd5203d53f894a04b4a6375fc8e
McNerdius <924403+McNerdius@users.noreply.github.com>
2019-10-10 15:53:07 -0500

    initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+
+when sharing a vscode screenshot the question "what font/theme is that" is almost inevitable.  enable this extension for screenshots to include that info in your statusbar.
+
+to install: code --install-extension themeinfo-0.0.1.vsix
---
+++ src/extension.ts
@@ -0,0 +1,22 @@
+
+import * as vscode from 'vscode';
+
+let statusBarItem: vscode.StatusBarItem;
+
+export function activate({ subscriptions }: vscode.ExtensionContext)
+{
+	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	subscriptions.push(statusBarItem);
+	subscriptions.push(vscode.workspace.onDidChangeConfiguration(updateStatusBarItem));
+	updateStatusBarItem();
+	statusBarItem.show();
+}
+
+function updateStatusBarItem(): void
+{
+	let font = vscode.workspace.getConfiguration("editor", null).get("fontFamily");
+	let theme = vscode.workspace.getConfiguration("workbench", null).get("colorTheme");
+	statusBarItem.text = `font: ${font} | theme: ${theme}`;
+}
+
+// export function deactivate() {}
