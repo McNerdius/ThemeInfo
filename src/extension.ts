be03080d1e7965a39ccbab03ff037831b3cb2e24
Jacob McDowell <McNerdius@live.com>
2019-12-17 23:49:56 -0700

    v0.7

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,121 @@
 
-import * as vscode from 'vscode';
+// pardon my C#-ish code stle :)
 
-let statusBarItem: vscode.StatusBarItem;
+import { ConfigurationTarget, workspace, window, StatusBarItem, ExtensionContext, StatusBarAlignment, commands } from 'vscode'
 
-export function activate({ subscriptions }: vscode.ExtensionContext)
+let _statusBarItem: StatusBarItem;
+let _context: ExtensionContext;
+
+const extensionName = "themeInfo";
+const titleSetting = `${extensionName}.windowTitle`;
+const templateSetting = `${extensionName}.template`;
+
+export function activate(context: ExtensionContext)
 {
-	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-	subscriptions.push(statusBarItem);
-	subscriptions.push(vscode.workspace.onDidChangeConfiguration(updateStatusBarItem));
+	_statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left, 100);
+
+	_context = context;
+	_context.subscriptions.push(_statusBarItem);
+
+	_context.subscriptions.push(workspace.onDidChangeConfiguration(updateStatusBarItem));
+	_context.subscriptions.push(workspace.onDidChangeConfiguration(updateWindowTitle));
+
+	_context.subscriptions.push(commands.registerCommand(`${extensionName}.addInfoGlobal`, addInfoGlobal));
+	_context.subscriptions.push(commands.registerCommand(`${extensionName}.addInfoWorkspace`, addInfoWorkspace));
+	_context.subscriptions.push(commands.registerCommand(`${extensionName}.showInfo`, showInfo));
+
 	updateStatusBarItem();
-	statusBarItem.show();
+	updateWindowTitle();
+}
+
+function addInfoGlobal(): void
+{
+	addInfo(ConfigurationTarget.Global);
+}
+
+function addInfoWorkspace(): void
+{
+	addInfo(ConfigurationTarget.Workspace);
+}
+
+function addInfo(scope: ConfigurationTarget): void
+{
+	const newTitle = getConfig("window.title") + "${separator}" + getConfig(templateSetting);
+	const config = workspace.getConfiguration();
+	config.update(titleSetting, newTitle, scope);
+
+	window.showInformationMessage("Edit themeInfo.windowTitle to change appearance if you want.");
+}
+
+function showInfo(): void
+{
+	const template = getConfig(templateSetting);
+	const message = applyVariables(template);
+	window.showInformationMessage(message);
 }
 
 function updateStatusBarItem(): void
 {
-	let font = vscode.workspace.getConfiguration("editor", null).get("fontFamily");
-	let theme = vscode.workspace.getConfiguration("workbench", null).get("colorTheme");
-	statusBarItem.text = `font: ${font} | theme: ${theme}`;
+	const template = getConfig(templateSetting);
+
+	if (template == "")
+	{
+		_statusBarItem.text = "";
+		_statusBarItem.hide();
+	}
+	else
+	{
+		_statusBarItem.text = applyVariables(template);
+		_statusBarItem.show();
+	}
 }
 
-// export function deactivate() {}
+function updateWindowTitle(): void
+{
+	const template = getConfig(titleSetting);
+
+	if (template == "")
+	{
+		return;
+	}
+
+	const scope = getScope(titleSetting);
+	const config = workspace.getConfiguration();
+	config.update("window.title", applyVariables(template), scope);
+}
+
+function applyVariables(template: string): string
+{
+	const font = getConfig("editor.fontFamily");
+	const theme = getConfig("workbench.colorTheme");
+
+	template = template.replace("${font}", font);
+	template = template.replace("${theme}", theme);
+
+	return template;
+}
+
+function getConfig(section: string): string
+{
+	// ?? causing issues loading extension, figure it out
+	return workspace.getConfiguration().get(section) || "";
+}
+
+function getScope(section: string): ConfigurationTarget
+{
+	const config = workspace.getConfiguration();
+	const info = config.inspect(section);
+
+	// ?. causing issues loading extension, figure it out
+	if (info == undefined) return ConfigurationTarget.Global;
+
+	if (info.workspaceFolderValue != null) return ConfigurationTarget.WorkspaceFolder;
+	else if (info.workspaceValue != null) return ConfigurationTarget.Workspace;
+	else return ConfigurationTarget.Global;
+}
+
+export function deactivate()
+{
+	_context.subscriptions.forEach(subscription => subscription.dispose())
+	_statusBarItem.dispose();
+}
